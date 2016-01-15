98c2254134a5bed83a86bfa4cadb22217c3b678a
Bryan Stoner <bryan.stoner@idfive.com>
2016-01-15 12:25:21 -0500

    prevent incorrect destination

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('compile-jade', function() {
     .pipe(inlineCSS({
       removeStyleTags: false
     }))
-    .pipe(gulp.dest(recursiveDest + 'html'));
+    .pipe(gulp.dest(recursiveDest));
 
 });
 
