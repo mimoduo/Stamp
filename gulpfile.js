1a33aad03b43df6521faf6720f9882412a9f1c61
Bryan Stoner <bryan.stoner@idfive.com>
2016-01-28 16:05:34 -0500

    reduce markup footprint

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     jade = require('gulp-jade'),
     inlineCSS = require('gulp-inline-css'),
     inlineSource = require('gulp-inline-source'),
+    prettify = require('gulp-prettify'),
     html2txt = require('gulp-html2txt');
 
 
@@ -23,6 +24,9 @@ gulp.task('compile-jade', function() {
     .pipe(inlineSource())
     .pipe(inlineCSS({
       removeStyleTags: false
+    }))
+    .pipe(prettify({
+      indent_size: 2
     }))
     .pipe(gulp.dest(recursiveDest));
 
